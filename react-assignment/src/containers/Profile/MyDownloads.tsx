811ec10d2803ea6938dc9f984e9af06dc9216926
VAJ-391999 <venishachauhan1234@gmail.com>
2021-06-03 18:34:55 +0530

    Practice 3/6

--- react-assignment/src/containers/Profile/MyDownloads.tsx
+++ react-assignment/src/containers/Profile/MyDownloads.tsx
@@ -3,14 +3,23 @@ import axios from 'axios';
 import Layout from '../../components/Layout/Layout';
 import './MyDownlods.css';
 import { makeStyles } from '@material-ui/core/styles';
+import { XGrid } from '@material-ui/x-grid';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
+import TableCell, { SortDirection } from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
+import { DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridValueFormatterParams,
+  GridSortDirection,
+  GridLinkOperator,
+  GridFilterModel,
+  GridToolbarContainer,
+  GridToolbarExport  } from '@material-ui/data-grid';
 
 const useStyles = makeStyles({
   table: {
@@ -18,27 +27,56 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+
+
+function GetFull(params: any) {
+  return `${params.getValue(params.id, 'firstname') || ''} ${params.getValue(params.id, 'lastename') || ''
+    }`;
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  {
+    field: 'firstname',
+    headerName: 'First Name',
+    width: 150
+  },
+  { field: 'lastename',
+    headerName: 'Last Name',
+    width: 150, resizable: true 
+  },
+  {
+    field: 'full',
+    headerName: 'Full Name',
+    width: 160, valueGetter: GetFull, sortComparator: (v1, v2, cellParams1, cellParams2) =>
+      GetFull(cellParams1).localeCompare(GetFull(cellParams2)),
+  },
+  { field: 'date',
+    headerName: 'Year',
+    width: 150,
+    valueFormatter: (params: GridValueFormatterParams) =>
+      (params.value as Date).getFullYear(),
+  }
 ];
 
-const rows1: GridRowsProp  = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'XGrid', col2: 'is Awesome' },
-  { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+
+
+const rows1: GridRowsProp = [
+  { id: 1, firstname: 'Mahesh', lastename: 'Katariya', date: new Date(1979, 0, 1) },
+  { id: 2, firstname: 'Bhola', lastename: 'Sharma', date: new Date(1971, 0, 1) },
+  { id: 3, firstname: 'Payal', lastename: 'Mehta', date: new Date(1979, 0, 1) },
+  { id: 4, firstname: 'Pretty', lastename: 'Gupta', date: new Date(1975, 0, 1) },
+  { id: 5, firstname: 'Mahesh', lastename: 'Katariya', date: new Date(1979, 0, 1) },
+  { id: 6, firstname: 'Bhola', lastename: 'Sharma', date: new Date(1971, 0, 1) },
+  { id: 7, firstname: 'Payal', lastename: 'Mehta', date: new Date(1979, 0, 1) },
+  { id: 8, firstname: 'Pretty', lastename: 'Gupta', date: new Date(1975, 0, 1) }
 ];
 
 
@@ -62,6 +100,25 @@ const MyDownloads = () => {
 
   }, []);
 
+  const filterModel: GridFilterModel = {
+    items: [
+      { columnField: 'lastename', operatorValue: 'contains', value: 'Gupta' },
+      { columnField: 'firstname', operatorValue: 'contains', value: 'Bhola' },
+    ],
+    linkOperator: GridLinkOperator.Or,
+  };
+
+  const sortModel = [
+    {
+      field: 'date',
+      sort: 'desc' as GridSortDirection,
+    },
+    {
+      field: 'firstname',
+      sort: 'asc' as GridSortDirection,
+    }
+  ]
+
 
 
 
@@ -69,36 +126,21 @@ const MyDownloads = () => {
     <Layout>
       <div className="MyDownloads">
         <h1>I am in Mydownloads</h1>
-        <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow key={row.name}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <h1>Table view with grid</h1>
-            <div style={{ height: 300, width: '100%', flexGrow: 1, display: 'flex' }}>
-              <DataGrid rows={rows1} columns={columns} />
-            </div>
+        
+        <h3>Table view with grid</h3>
+        <div style={{ height: 300, width: '20%', flexGrow: 1, display: 'flex'  }}>
+          <XGrid 
+            rows={rows1}
+            columns={columns}
+            rowHeight={30}
+            sortingOrder={['desc', 'asc']}
+            sortModel={sortModel}
+            filterModel={filterModel}
+            pageSize={200}
+            columnBuffer={2}
+            
+            />
+        </div>
       </div>
     </Layout>
   );

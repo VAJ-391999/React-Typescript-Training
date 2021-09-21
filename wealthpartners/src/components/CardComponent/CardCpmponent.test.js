1107ae5b2f20900de0e0b47a97102ddcffaf9adf
VAJ-391999 <venishachauhan1234@gmail.com>
2021-09-21 12:06:57 +0530

    practice 21/09

--- wealthpartners/src/components/CardComponent/CardCpmponent.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CardComponent from './CardComponent';
-
-test('render', () => {
-    render(<CardComponent title="Life Product" text="hello customer" imgcomponent="" />)
-    expect()
-})
